7f357143282acef696138d67069e600d2ff61bac
Aaron Whitney <aaron@room19apps.com>
2013-05-14 20:48:53 -0500

    Initial commit.

---
+++ PracticeRailsApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PracticeRailsApp::Application.config.secret_token = '463de40b00307c88d2f78ab8862acca2bce6ec4cf779b9db894e59e6473c850406993328a063b2dd27b23bb475270f6e6e606c6810da3791b7bc4c7c1fa70a62'
